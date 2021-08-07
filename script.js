ab26c9938f094140e228e5cc2938bda59e34df96
Sloth247 <86727485+Sloth247@users.noreply.github.com>
2021-08-08 07:22:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // rotate arrow
 
-const menuLinks = document.querySelectorAll(".menu--link");
-const arrows = document.querySelectorAll(".nav-bar__arrow img");
-const modals = document.querySelectorAll(".dropdown");
+const menuLinks = document.querySelectorAll(".menu-btn");
+const arrows = document.querySelectorAll(".nav-bar__arrow");
+const dropdownModals = document.querySelectorAll(".dropdown");
 
 const mediaQuery = window.matchMedia("(min-width: 56.3em)");
 
 function modalClose(e) {
-  modals.forEach((modal, idx) => {
+  dropdownModals.forEach((modal, idx) => {
     if (e.matches) {
+      menuLinks[idx].setAttribute("aria-expanded", "false");
+      dropdownModals[idx].setAttribute("aria-hidden", "true");
       arrows[idx].classList.remove("rotate-arrow");
-      modals[idx].classList.remove("active");
     }
   });
 }
@@ -19,32 +20,70 @@ mediaQuery.addEventListener("change", modalClose);
 
 menuLinks.forEach((menulink, idx) => {
   menulink.addEventListener("click", () => {
-    arrows[idx].classList.toggle("rotate-arrow");
-    modals[idx].classList.toggle("active");
-  });
-  menulink.addEventListener("focus", () => {
-    arrows[idx].classList.toggle("rotate-arrow");
-    modals[idx].classList.toggle("active");
-  });
-  menulink.addEventListener("mouseover", () => {
+    let dropdownOpen =
+      dropdownModals[idx].getAttribute("aria-hidden") == "false";
+    let menuExpanded = menuLinks[idx].getAttribute("aria-expanded") == "true";
+
+    menuLinks[idx].setAttribute("aria-expanded", "true");
+    dropdownModals[idx].setAttribute("aria-hidden", "false");
     arrows[idx].classList.add("rotate-arrow");
-    modals[idx].classList.add("active");
+    /* dropdownModals[idx].classList.toggle("active");*/
+
+    if (dropdownOpen && menuExpanded) {
+      menuLinks[idx].setAttribute("aria-expanded", "false");
+      dropdownModals[idx].setAttribute("aria-hidden", "true");
+      arrows[idx].classList.remove("rotate-arrow");
+    }
   });
+
+  /* menulink.addEventListener("focus", () => {
+    let dropdownOpen =
+      dropdownModals[idx].getAttribute("aria-hidden") == "false";
+    let menuExpanded = menuLinks[idx].getAttribute("aria-expanded") == "true";
+
+    menuLinks[idx].setAttribute("aria-expanded", "true");
+    dropdownModals[idx].setAttribute("aria-hidden", "false");
+    arrows[idx].classList.add("rotate-arrow");
+    /* dropdownModals[idx].classList.toggle("active");*/
+  /*
+    if (dropdownOpen && menuExpanded) {
+      menuLinks[idx].setAttribute("aria-expanded", "false");
+      dropdownModals[idx].setAttribute("aria-hidden", "true");
+      arrows[idx].classList.remove("rotate-arrow");
+    }
+  })*/
+
+  /* menulink.addEventListener("mouseover", () => {
+    arrows[idx].classList.add("rotate-arrow");
+    dropdownModals[idx].classList.add("active");
+  }); */
 });
 
-modals.forEach((modal, idx) => {
+/* dropdownModals.forEach((modal, idx) => {
   modal.addEventListener("mouseout", () => {
     arrows[idx].classList.remove("rotate-arrow");
-    modals[idx].classList.remove("active");
+    dropdownModals[idx].classList.remove("active");
   });
-});
+}); */
 
 // Mobile hamburger menu
 const hamburger = document.querySelector(".nav-bar__hamburger-button");
 const mobileMenu = document.querySelector(".nav-bar__menu");
 
 hamburger.addEventListener("click", () => {
-  mobileMenu.classList.toggle("modal-mobile");
+  const modalOpen = mobileMenu.getAttribute("aria-hidden") == "false";
+  const btnClicked = hamburger.getAttribute("aria-expanded") == "true";
+
+  mobileMenu.setAttribute("aria-hidden", "false");
+  hamburger.setAttribute("aria-expanded", "true");
+  hamburger.classList.add("is-active");
+
+  if (modalOpen && btnClicked) {
+    mobileMenu.setAttribute("aria-hidden", "true");
+    hamburger.setAttribute("aria-expanded", "false");
+    hamburger.classList.remove("is-active");
+  }
+
   /* upperClose.classList.add("rotate-top");
   middleClose.style.opacity = 0;
   bottomClose.classList.add("rotate-bottom"); */
